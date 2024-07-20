7519ee25b5df274ea50f3bfefd4e75ed60f525f5
alkan arslan <arslanalkan356@gmail.com>
2024-07-20 14:38:39 +0300

    subscribe area completed

--- AcunMedyaFurnitureProject/Controllers/UILayoutController.cs
+++ AcunMedyaFurnitureProject/Controllers/UILayoutController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AcunMedyaFurnitureProject.DataAccess.Context;
+using AcunMedyaFurnitureProject.DataAccess.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AcunMedyaFurnitureProject.Controllers
 {
     public class UILayoutController : Controller
     {
+        private readonly FurniterContext _context;
+
+        public UILayoutController(FurniterContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        public PartialViewResult Subscribe()
+        {
+            var subscriber = new Subscriber();
+            return PartialView(subscriber);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Subscribe(Subscriber subscriber)
+        {
+            if (ModelState.IsValid)
+            {
+                // Abone oluşturma işlemini gerçekleştir
+                _context.Subscribers.Add(subscriber);
+                await _context.SaveChangesAsync();
+
+                // Abone kimliğini al
+                int subscriberId = subscriber.SubscriberID;
+                TempData["SubscriberID"] = subscriberId; // Kimliği TempData'ya kaydedin
+                return RedirectToAction("Index", "Default");
+            }
+            return View(subscriber);
+        }
+
+        public IActionResult SubscribeSuccess()
+        {
+            if (TempData["SubscriberID"] != null)
+            {
+                ViewBag.SubscriberID = TempData["SubscriberID"];
+                return View();
+            }
+            return RedirectToAction("Subscribe");
         }
     }
 }
--- AcunMedyaFurnitureProject/ViewComponents/UILayout/_UILayoutFooterComponent.cs
+++ AcunMedyaFurnitureProject/ViewComponents/UILayout/_UILayoutFooterComponent.cs
@@ -6,13 +6,7 @@ namespace AcunMedyaFurnitureProject.ViewComponents.UILayout
 {
     public class _UILayoutFooterComponent : ViewComponent
     {
-        private readonly FurniterContext _context;
-
-        public _UILayoutFooterComponent(FurniterContext context)
-        {
-            _context = context;
-        }
-
+      
         public IViewComponentResult Invoke()
         {
             return View();
